36abafc2c3c41b3048160ca63c7d5118df8d61f6
Marc Miller <marcjmiller@outlook.com>
2021-01-13 21:45:10 -0500

    init skeleton react-app, install dependencies

---
+++ craco.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig, {env, paths}) => {
+      return {
+        ...webpackConfig,
+        // entry: {
+        //   main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(Boolean),
+        //   content: './src/chrome/content.ts',
+        // },
+        // output: {
+        //   ...webpackConfig.output,
+        //   filename: 'static/js/[name].js',
+        // },
+        optimization: {
+          ...webpackConfig.optimization,
+          runtimeChunk: false,
+        }
+      }
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className='app-container flex flex-col text-center justify-around items-center min-h-screen min-w-screen'>
+    <div className={'header-container text-5xl'}>chrome-simple-start</div>
+    <div className={'greeting-container'}>A message goes here</div>
+    <div className={'search-container'}>A search goes here</div>
+    <div className={'resource-container flex flex-row justify-around min-w-full'}>
+      <div className={'resource-header'}>
+        header
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+      </div>
+      <div className={'resource-header'}>
+        header
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+      </div>
+      <div className={'resource-header'}>
+        header
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+      </div>
+      <div className={'resource-header'}>
+        header
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+      </div>
+      <div className={'resource-header'}>
+        header
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+        <div className={'resource'}>resource link</div>
+      </div>
     </div>
-  );
-}
+    <div className={'date-time-container'}></div>
+    <div className={'footer-container'}>
+      <p className={'xs'}>
+        inspired by deepjyoti's{' '}
+        <a className={'text-red-500'} href='https://github.com/deepjyoti30/startpage.git'>
+          startpage
+        </a>
+      </p>
+    </div>
+  </div>
+);
 
 export default App;
