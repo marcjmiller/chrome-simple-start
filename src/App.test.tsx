231258b74d5e2195167894b472adc2b8563a2558
Marc Miller <marcjmiller@outlook.com>
2021-01-13 22:05:28 -0500

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,41 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App.tsx tests', () => {
+  
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('should render the greeter text', () => {
+    const greetingContainer = screen.getByText(/chrome-simple-start/i);
+    expect(greetingContainer).toBeInTheDocument();
+  });
+
+  it('should render a clock container', () => {
+    const clockContainer = screen.getByTestId('clock-container');
+    expect(clockContainer).toBeInTheDocument();
+  })
+  
+  it('should render a search container', () => {
+    const searchContainer = screen.getByTestId('search-container');
+    expect(searchContainer).toBeInTheDocument();
+  })
+  
+  it('should render a resource container', () => {
+    const resourceContainer = screen.getByTestId('resource-container');
+    expect(resourceContainer).toBeInTheDocument();
+  })
+  
+  it('should render 5 resources', () => {
+    const resourceElement = screen.getAllByTestId('resource');
+    expect(resourceElement.length).toBe(5);
+  })
+  
+  it('should render a footer container', () => {
+    const footerContainer = screen.getByTestId('footer-container');
+    expect(footerContainer).toBeInTheDocument();
+  })
+  
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,41 @@ const App = () => (
   <div className='app-container flex flex-col text-center justify-around items-center min-h-screen min-w-screen'>
     <div className={'header-container text-5xl'}>chrome-simple-start</div>
     <div className={'greeting-container'}>A message goes here</div>
-    <div className={'search-container'}>A search goes here</div>
-    <div className={'resource-container flex flex-row justify-around min-w-full'}>
-      <div className={'resource-header'}>
+    <div className={'clock-container'} data-testid={'clock-container'}>A clock goes here</div>
+    <div className={'search-container'} data-testid={'search-container'}>A search goes here</div>
+    <div className={'resource-container flex flex-row justify-around min-w-full'} data-testid={'resource-container'}>
+      <div className={'resource-header'} data-testid={'resource'}>
         header
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
       </div>
-      <div className={'resource-header'}>
+      <div className={'resource-header'} data-testid={'resource'}>
         header
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
       </div>
-      <div className={'resource-header'}>
+      <div className={'resource-header'} data-testid={'resource'}>
         header
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
       </div>
-      <div className={'resource-header'}>
+      <div className={'resource-header'} data-testid={'resource'}>
         header
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
       </div>
-      <div className={'resource-header'}>
+      <div className={'resource-header'} data-testid={'resource'}>
         header
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
         <div className={'resource'}>resource link</div>
       </div>
     </div>
-    <div className={'date-time-container'}></div>
-    <div className={'footer-container'}>
+    <div className={'footer-container'} data-testid={'footer-container'}>
       <p className={'xs'}>
         inspired by deepjyoti's{' '}
         <a className={'text-red-500'} href='https://github.com/deepjyoti30/startpage.git'>
