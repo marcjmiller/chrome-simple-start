58b03b9a10b92d579e0e6eae49b030f2b88994f1
Marc Miller <marcjmiller@outlook.com>
2021-01-19 17:58:46 -0500

    add LICENSE, Icons, tweak config.ts, update App.tsx for start of config menu/options

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Clock from './Clock';
 import Footer from './Footer';
@@ -8,16 +8,25 @@ import Resources from './Resources';
 import Search from './Search';
 
 import config, { IResource } from '../config/config';
+import Icons from './Icons';
 
 const App: React.FC = () => {
+  const [showSettings, setShowSettings] = useState(false);
+
   return (
     <div className='app-container bg-white dark:bg-gray-900 text-gray-900 dark:text-white flex flex-col text-center justify-around items-center min-h-screen min-w-screen'>
-      <Header />
-      <Greeter username={config.username} />
-      <Clock />
-      <Search />
+      <div className={''}>
+        <Icons name={'cog'} className={'absolute top-1 right-1'} onClick={() => setShowSettings(!showSettings)} />
+        {showSettings && (
+          <div className={'absolute top-10 right-1 p-4 mx-4 bg-gray-600 rounded-lg '}>Settings shown!</div>
+        )}
+      </div>
+      {!config.hideHeader && <Header />}
+      {!config.hideGreeter && <Greeter username={config.username} />}
+      {!config.hideClock && <Clock />}
+      {!config.hideSearch && <Search />}
       <Resources resources={config.resources.length > 0 ? config.resources : ([] as IResource[])} />
-      <Footer />
+      {!config.hideFooter && <Footer />}
     </div>
   );
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,10 @@
 export interface IConfig {
   username: string;
+  hideClock: boolean;
+  hideFooter: boolean;
+  hideGreeter: boolean;
+  hideHeader: boolean;
+  hideSearch: boolean;
   resources: IResource[];
 }
 
@@ -8,53 +13,58 @@ export interface IResource {
   links: { text: string; url: string }[];
 }
 
-const config = {
-  'username': 'marcjmiller',
-  'resources': [
+const config: IConfig = {
+  username: 'marcjmiller',
+  hideClock: false,
+  hideFooter: false,
+  hideGreeter: false,
+  hideHeader: false,
+  hideSearch: false,
+  resources: [
     {
-      'category': 'heading 1',
-      'links': [
-        { "text": 'link 1', "url": 'http://www.google.com' },
-        { "text": 'link 2', 'url': 'http://www.google.com' },
-        { "text": 'link 3', 'url': 'http://www.google.com' },
+      category: 'heading 1',
+      links: [
+        { text: 'link 1', url: 'http://www.google.com' },
+        { text: 'link 2', url: 'http://www.google.com' },
+        { text: 'link 3', url: 'http://www.google.com' },
       ],
     },
     {
-      'category': 'heading 2',
-      'links': [
-        { 'text': 'link 1', 'url': 'http://www.google.com' },
-        { 'text': 'link 2', 'url': 'http://www.google.com' },
-        { 'text': 'link 3', 'url': 'http://www.google.com' },
-        { 'text': 'link 4', 'url': 'http://www.google.com' },
-        { 'text': 'link 5', 'url': 'http://www.google.com' },
+      category: 'heading 2',
+      links: [
+        { text: 'link 1', url: 'http://www.google.com' },
+        { text: 'link 2', url: 'http://www.google.com' },
+        { text: 'link 3', url: 'http://www.google.com' },
+        { text: 'link 4', url: 'http://www.google.com' },
+        { text: 'link 5', url: 'http://www.google.com' },
       ],
     },
     {
-      'category': 'heading 3',
-      'links': [
-        { 'text': 'link 1', 'url': 'http://www.google.com' },
-        { 'text': 'link 2', 'url': 'http://www.google.com' },
-        { 'text': 'link 3', 'url': 'http://www.google.com' },
+      category: 'heading 3',
+      links: [
+        { text: 'link 1', url: 'http://www.google.com' },
+        { text: 'link 2', url: 'http://www.google.com' },
+        { text: 'link 3', url: 'http://www.google.com' },
       ],
     },
     {
-      'category': 'heading 4',
-      'links': [
-        { 'text': 'link 1', 'url': 'http://www.google.com' },
-        { 'text': 'link 2', 'url': 'http://www.google.com' },
-        { 'text': 'link 3', 'url': 'http://www.google.com' },
-        { 'text': 'link 4', 'url': 'http://www.google.com' },
+      category: 'heading 4',
+      links: [
+        { text: 'link 1', url: 'http://www.google.com' },
+        { text: 'link 2', url: 'http://www.google.com' },
+        { text: 'link 3', url: 'http://www.google.com' },
+        { text: 'link 4', url: 'http://www.google.com' },
       ],
     },
     {
-      'category': 'heading 5',
-      'links': [
-        { 'text': 'link 1', 'url': 'http://www.google.com' },
-        { 'text': 'link 2', 'url': 'http://www.google.com' },
-        { 'text': 'link 3', 'url': 'http://www.google.com' },
-        { 'text': 'link 4', 'url': 'http://www.google.com' },
-        { 'text': 'link 5', 'url': 'http://www.google.com' },
-        { 'text': 'link 6', 'url': 'http://www.google.com' },
+      category: 'heading 5',
+      links: [
+        { text: 'link 1', url: 'http://www.google.com' },
+        { text: 'link 2', url: 'http://www.google.com' },
+        { text: 'link 3', url: 'http://www.google.com' },
+        { text: 'link 4', url: 'http://www.google.com' },
+        { text: 'link 5', url: 'http://www.google.com' },
+        { text: 'link 6', url: 'http://www.google.com' },
       ],
     },
   ],
