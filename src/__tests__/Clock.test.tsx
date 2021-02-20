6c3c505736551180e8012381d26ce3a586426658
Marc Miller <marcjmiller@outlook.com>
2021-02-20 18:22:52 -0500

    update clock component to show current date/time.

--- src/__tests__/Clock.test.tsx
+++ src/__tests__/Clock.test.tsx
@@ -5,8 +5,10 @@ import Clock from '../components/Clock';
 
 describe('Clock tests', () => {
   it('should render a clock container', () => {
-    render(<Clock />);
+    render(<Clock dateInput={new Date('December 17, 1995 03:24:00')} />);
     const clockElement = screen.getByTestId('clock-container');
+    const clockString = screen.getByText(/It is currently 3:24 AM on 17 December/i)
     expect(clockElement).toBeInTheDocument();
+    expect(clockString).toBeInTheDocument();
   });
 });
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,7 +1,28 @@
-const Clock = () => {
+import { useEffect, useState } from 'react';
+
+interface OwnProps {
+  dateInput?: Date;
+}
+
+const Clock = ({ dateInput }: OwnProps) => {
+  const [date, setDate] = useState(dateInput ? dateInput : new Date());
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  });
+
+  const niceDate = new Intl.DateTimeFormat('en-GB', { day: 'numeric', month: 'long' }).format(date);
+  const hour = date.getHours();
+  const niceHr = hour > 12 ? hour - 12 : hour;
+  const mins = date.getMinutes().toString().padStart(2, '0');
+  const ampm = hour > 12 ? 'PM' : 'AM';
+
   return (
     <div className={'clock-container'} data-testid={'clock-container'}>
-      A clock goes here
+      It is currently {niceHr}:{mins} {ampm} on {niceDate}.
     </div>
   );
 };
